019832db11d8c7ebd173c9dc3079245a00668e52
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-07-07 19:54:59 -0300

    Chapter 5

---
+++ chapter-5/toppings.py
@@ -0,0 +1,10 @@
+requested_topping = "mushrooms"
+
+requested_toppings = ["mushrooms", "onion"]
+
+print("onion" in requested_toppings)
+print("onion" not in requested_toppings)
+
+
+if requested_topping != "anchovies":
+    print("Hold the anchovies!")
